8b69026b710086fb47ca970430042e6580348f7e
Nite Luo <darkdukey@gmail.com>
2015-08-11 16:25:46 -0700

    Finish ios

--- js/src/app.js
+++ js/src/app.js
@@ -2,15 +2,19 @@
 var HelloWorldLayer = cc.Layer.extend({
     sprite:null,
     ctor:function () {
-        //////////////////////////////
-        // 1. super init first
+
         this._super();
 
-        /////////////////////////////
-        // 2. add a menu item with "X" image, which is clicked to quit the program
-        //    you may modify it.
-        // ask the window size
+        sdkbox.PluginGoogleAnalytics.init();
+
         var size = cc.winSize;
+
+        var logEventItem = new cc.MenuItemFont("LogEvent", function(){
+          sdkbox.PluginGoogleAnalytics.logEvent("TestCategory", "Action", "Event", 1);
+          sdkbox.PluginGoogleAnalytics.dispatchHits();
+        }, this);
+        var m = new cc.Menu(logEventItem);
+        this.addChild(m);
 
         // add a "close" icon to exit the progress. it's an autorelease object
         var closeItem = new cc.MenuItemImage(
@@ -31,39 +35,6 @@ var HelloWorldLayer = cc.Layer.extend({
         menu.y = 0;
         this.addChild(menu, 1);
 
-        /////////////////////////////
-        // 3. add your codes below...
-        // add a label shows "Hello World"
-        // create and initialize a label
-        var helloLabel = new cc.LabelTTF("Hello World", "Arial", 38);
-        // position the label on the center of the screen
-        helloLabel.x = size.width / 2;
-        helloLabel.y = 0;
-        // add the label as a child to this layer
-        this.addChild(helloLabel, 5);
-
-        // add "HelloWorld" splash screen"
-        this.sprite = new cc.Sprite(res.HelloWorld_png);
-        this.sprite.attr({
-            x: size.width / 2,
-            y: size.height / 2,
-            scale: 0.5,
-            rotation: 180
-        });
-        this.addChild(this.sprite, 0);
-
-        this.sprite.runAction(
-            cc.sequence(
-                cc.rotateTo(2, 0),
-                cc.scaleTo(2, 1, 1)
-            )
-        );
-        helloLabel.runAction(
-            cc.spawn(
-                cc.moveBy(2.5, cc.p(0, size.height - 40)),
-                cc.tintTo(2.5,255,125,0)
-            )
-        );
         return true;
     }
 });
